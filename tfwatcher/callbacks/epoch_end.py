4a1e44c9946007bbb35901d16a1bea40a28de632
Rishit Dagli <rishit.dagli@gmail.com>
2021-08-02 15:30:11 +0530

    Monitor per epoch time taken

--- tfwatcher/callbacks/epoch_end.py
+++ tfwatcher/callbacks/epoch_end.py
@@ -7,6 +7,8 @@ class EpochEnd(tf.keras.callbacks.Callback):
     def __init__(self, schedule: Union[int, list]):
         super(EpochEnd, self).__init__()
         self.schedule = schedule
+        self.start_time = None
+        self.end_time = None
 
         self.is_int = False
         self.is_list = False
@@ -29,7 +31,11 @@ class EpochEnd(tf.keras.callbacks.Callback):
                     )
                 )
 
+    def on_epoch_begin(self, epoch, logs=None):
+        self.start_time = tf.timestamp()
+
     def on_epoch_end(self, epoch: int, logs: dict = None):
+        self.end_time = tf.timestamp()
         if self.is_int:
             if (epoch + 1) % self.schedule == 0:
                 data = logs
@@ -38,3 +44,7 @@ class EpochEnd(tf.keras.callbacks.Callback):
             if (epoch + 1) in self.schedule:
                 data = logs
                 data["epoch"] = epoch
+
+        # Use Python built in functions to allow using in @tf.function see
+        # https://github.com/tensorflow/tensorflow/issues/27491#issuecomment-890887810
+        float(self.end_time - self.start_time)
