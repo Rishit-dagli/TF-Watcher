5ffcc437faf1d1c3430e4a0c64c27dd412472597
Rishit Dagli <rishit.dagli@gmail.com>
2021-08-03 22:54:28 +0530

    Add type annotations

--- tfwatcher/firebase_helpers.py
+++ tfwatcher/firebase_helpers.py
@@ -3,12 +3,12 @@ import pyrebase
 from .firebase_config import get_firebase_config
 
 
-def write_to_firebase(data, config, id, level):
+def write_to_firebase(data: dict, config: dict, ref_id: str, level: str):
     # level can be epoch, batch, prediction
     firebase = pyrebase.initialize_app(config)
     log_db = firebase.database()
 
     if level == "prediction":
-        log_db.child(id).child(1).push(data)
+        log_db.child(ref_id).child(1).push(data)
     else:
-        log_db.child(id).child(data[level]).push(data)
+        log_db.child(ref_id).child(data[level]).push(data)
