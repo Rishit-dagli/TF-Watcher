56a6c95684d0d3bcf1fae8449348b3aaa3be42a2
Rishit Dagli <rishit.dagli@gmail.com>
2021-08-04 13:17:25 +0530

    Add `__all__` attribute

--- tfwatcher/__init__.py
+++ tfwatcher/__init__.py
@@ -1,3 +1,8 @@
 from .version import __version__
+from .callbacks import epoch
+from .callbacks import predict_batch
+from .callbacks import predict
+from .callbacks import predict_batch
+from .callbacks import train_batch
 
 __all__ = ["firebase_config", "firebase_helpers", "callbacks"]
--- tfwatcher/callbacks/__init__.py
+++ tfwatcher/callbacks/__init__.py
@@ -1,1 +1,7 @@
+from .epoch import EpochEnd
+from .predict_batch import PredictBatchEnd
+from .train_batch import TrainBatchEnd
+from .test_batch import TestBatchEnd
+from .predict import PredictEnd
+
 __all__ = ["epoch", "predict", "predict_batch", "test_batch", "train_batch"]
