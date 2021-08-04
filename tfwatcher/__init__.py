6f560aeda923f923eda05c223504dce448cda46d
Rishit Dagli <rishit.dagli@gmail.com>
2021-08-04 08:08:02 +0000

    Reformat with isort

--- tfwatcher/__init__.py
+++ tfwatcher/__init__.py
@@ -1,5 +1,4 @@
 from .callbacks import epoch, predict, predict_batch, train_batch
 from .version import __version__
 
-
 __all__ = ["firebase_config", "firebase_helpers", "callbacks"]
--- tfwatcher/callbacks/__init__.py
+++ tfwatcher/callbacks/__init__.py
@@ -4,5 +4,4 @@ from .predict_batch import PredictBatchEnd
 from .test_batch import TestBatchEnd
 from .train_batch import TrainBatchEnd
 
-
 __all__ = ["epoch", "predict", "predict_batch", "test_batch", "train_batch"]
