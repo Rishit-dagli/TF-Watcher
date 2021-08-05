73d38a6b918bafb84a568942592906d890b57f09
Rishit Dagli <rishit.dagli@gmail.com>
2021-08-05 04:28:23 +0000

    Write docstrings `write_in_callback()`

--- tfwatcher/firebase_helpers.py
+++ tfwatcher/firebase_helpers.py
@@ -7,11 +7,13 @@ from .firebase_config import get_firebase_config
 
 
 def write_to_firebase(data: dict, ref_id: str, level: str) -> None:
-    """Writes data to Firebase using `https://github.com/thisbejim/Pyrebase <https://stackoverflow.com/a/37484053/11878567>`_
-    , a simple Python wrapper around the Firebase API.
+    """Writes data to Firebase Realtime Database using 
+    `https://github.com/thisbejim/Pyrebase <https://stackoverflow.com/a/37484053/11878567>`_
+    , a simple Python wrapper around the Firebase API. This automatically fetches the 
+    Firebase Config from :func:`firebase_config.get_firebase_config` .
 
-    :param data: A dictionary of Firebase project configuration from
-        :func:`firebase_config.get_firebase_config`
+    :param data: A dictionary of the logging metrics, epoch number and average time 
+        which are to be logged to Firebase 
     :type data: dict
     :param ref_id: A unique ID where the data would be pushed to on Firebase
     :type ref_id: str
@@ -32,6 +34,19 @@ def write_to_firebase(data: dict, ref_id: str, level: str) -> None:
 
 
 def write_in_callback(data: dict, ref_id: str):
+    """A wrapper around :func:`firebase_helpers.write_to_firebase` to simply pass in 
+    the ``data`` and a unique ID to write to Firebase Realtime database. It 
+    automatically figures out the level at which logs were collected and calls the 
+    :func:`firebase_helpers.write_to_firebase` function. This function is also used to 
+    write data to Firebase in between callbacks (eg. the :class:`EpochEnd` class). 
+
+    :param data: A dictionary of the logging metrics, epoch number and average time 
+        which are to be logged to Firebase 
+    :type data: dict
+    :param ref_id: A unique ID where the data would be pushed to on Firebase
+    :type ref_id: str
+    """
+
     if data["epoch"]:
         level = "epoch"
     elif data["batch"]:
@@ -43,4 +58,15 @@ def write_in_callback(data: dict, ref_id: str):
 
 
 def random_char(y: int) -> str:
+    """A very simple function to help generate an arbitary length of pseudo random 
+    letters to serve as a unique ID specific to the class through which metrics are 
+    being logged. This is also the child under which the mtrics are logged in Firebase 
+    Realtime database.
+
+    :param y: The length of the unique ID to be created
+    :type y: int
+    :return: A string of ``y`` pseudo random upper case and lower letters
+    :rtype: str
+    """
+
     return "".join(random.choice(string.ascii_letters) for _ in range(y))
