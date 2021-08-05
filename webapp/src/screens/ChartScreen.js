2b9f74f2735d225e53168ee13ca83aeca724f800
unknown <srparadkar15@gmail.com>
2021-08-05 15:05:37 +0530

    Remove auth functionality from screens

--- webapp/src/screens/ChartScreen.js
+++ webapp/src/screens/ChartScreen.js
@@ -1,34 +1,12 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { Redirect, useHistory } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import {
-  Button, VStack, Text, useToast,
+  VStack, Text,
 } from '@chakra-ui/react';
 import { UserContext } from '../providers/AuthProvider';
-import { logOut } from '../firebase/Firebase';
 
 const ChartScreen = () => {
-  const history = useHistory();
   const { user, loading } = useContext(UserContext);
-  const [err, setErr] = useState(false);
-  const toast = useToast();
-
-  const logout = async () => {
-    const success = await logOut();
-    if (success) history.go(0);
-    else setErr(!success);
-  };
-
-  useEffect(() => {
-    if (err) {
-      toast({
-        title: 'Error',
-        description: 'Something went wrong! Please try again',
-        status: 'error',
-        duration: 8000,
-        isClosable: true,
-      });
-    }
-  }, [err]);
 
   return (
     <>
@@ -37,7 +15,6 @@ const ChartScreen = () => {
       ) : (
         <VStack>
           <Text fontSize="3xl">Chart screen</Text>
-          <Button onClick={logout}>Logout</Button>
         </VStack>
       )}
     </>
--- webapp/src/screens/HomeScreen.js
+++ webapp/src/screens/HomeScreen.js
@@ -1,56 +1,25 @@
 import React, { useState, useContext, useEffect } from 'react';
 import {
-  Button, VStack, Text, useToast,
+  Button, VStack, Text,
 } from '@chakra-ui/react';
 import { useHistory } from 'react-router-dom';
-import { logOut, signInWithGoogle } from '../firebase/Firebase';
 import { UserContext } from '../providers/AuthProvider';
 
 const HomeScreen = () => {
   const history = useHistory();
   const { user } = useContext(UserContext);
   const [loginStatus, setLoginStatus] = useState(false);
-  const [err, setErr] = useState(false);
-  const toast = useToast();
-
-  const login = async () => {
-    const success = await signInWithGoogle();
-    setErr(!success);
-  };
-
-  const logout = async () => {
-    const success = await logOut();
-    if (success) history.go(0);
-    else setErr(!success);
-  };
 
   useEffect(() => {
     if (user) setLoginStatus(true);
   }, [user]);
 
-  useEffect(() => {
-    if (err) {
-      toast({
-        title: 'Error',
-        description: 'Something went wrong! Please try again',
-        status: 'error',
-        duration: 8000,
-        isClosable: true,
-      });
-    }
-  }, [err]);
-
   return (
     <VStack>
-      <Text fontSize="3xl">Home screen</Text>
+      {/* <Text fontSize="3xl">Home screen</Text> */}
       { loginStatus
-        ? (
-          <>
-            <Button onClick={logout}>Logout</Button>
-            <Button onClick={() => history.push('/logs')}>View logs</Button>
-          </>
-        )
-        : <Button onClick={login}>Login</Button>}
+        ? <Button onClick={() => history.push('/logs')}>View logs</Button>
+        : <Text fontSize="2xl">Login to get started!</Text>}
     </VStack>
   );
 };
