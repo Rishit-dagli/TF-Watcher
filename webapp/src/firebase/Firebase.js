ea7a97fd19264d377941c04c1f889309c6482d9e
unknown <srparadkar15@gmail.com>
2021-08-05 16:54:59 +0530

    Get data from firebase

--- webapp/src/firebase/Firebase.js
+++ webapp/src/firebase/Firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -9,6 +10,7 @@ firebase.initializeApp({
 });
 
 export const auth = firebase.auth();
+export const db = firebase.database();
 
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
--- webapp/src/screens/ChartScreen.js
+++ webapp/src/screens/ChartScreen.js
@@ -1,12 +1,37 @@
-import React, { useContext } from 'react';
+import React, {
+  useContext, useState, useEffect,
+} from 'react';
 import { Redirect } from 'react-router-dom';
 import {
   VStack, Text,
 } from '@chakra-ui/react';
+
 import { UserContext } from '../providers/AuthProvider';
+import { db } from '../firebase/Firebase';
 
 const ChartScreen = () => {
   const { user, loading } = useContext(UserContext);
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    db.ref('jGNGTBd').on('value', (snapshot) => {
+      const getAllData = [];
+      snapshot.forEach((snap) => {
+        getAllData.push(snap.val());
+      });
+      const newLogs = [];
+      getAllData.forEach((element) => {
+        Object.keys(element).map((x) => {
+          newLogs.push(element[x]);
+          return null;
+        });
+      });
+      setLogs(newLogs);
+    });
+  }, []);
+
+  // eslint-disable-next-line no-console
+  console.log(logs);
 
   return (
     <>
