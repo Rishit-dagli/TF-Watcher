19cb7dcb79de22d8dd53d638de3d202df863bd86
unknown <srparadkar15@gmail.com>
2021-08-05 15:05:06 +0530

    Add navbar

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import UserProvider from './providers/AuthProvider';
 import ChartScreen from './screens/ChartScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -7,6 +8,7 @@ import HomeScreen from './screens/HomeScreen';
 const App = () => (
   <UserProvider>
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route path="/logs" component={ChartScreen} />
         <Route exact path="/" component={HomeScreen} />
