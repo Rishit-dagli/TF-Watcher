14cfd5459d7f5ee712fd307fd4595d8d85cc9891
Rishit Dagli <rishit.dagli@gmail.com>
2021-08-05 03:43:34 +0000

    Add docstrings for `get_firebase_config()`

--- tfwatcher/firebase_config.py
+++ tfwatcher/firebase_config.py
@@ -1,7 +1,12 @@
-# It is safe to expose Firebase apiKey publicly, read: https://stackoverflow.com/a/37484053/11878567
-
-
 def get_firebase_config() -> dict:
+    """Returns a dictionary to initialize Firebase containing registered app's
+    Firebase project configuration. It is safe to expose Firebase apiKey
+    publicly, read `this Stack Overflow answer https://stackoverflow.com/a/37484053/11878567`_ .
+
+    :return: A dictionary of Firebase project configuration
+    :rtype: dict
+    """
+
     # It is better using a dict literal, read: https://stackoverflow.com/a/6610783/11878567
     return {
         "apiKey": "AIzaSyAfCOOzFtKxTa-_pS3lO6URRGR8sVjK7sk",
