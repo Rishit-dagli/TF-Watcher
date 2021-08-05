d669c7a7dafb6b540b03b69a2fc7cb462833bc20
Rishit Dagli <rishit.dagli@gmail.com>
2021-08-05 04:10:46 +0000

    Write docstrings for `write_to_firebase()`

--- tfwatcher/firebase_helpers.py
+++ tfwatcher/firebase_helpers.py
@@ -6,7 +6,21 @@ import pyrebase
 from .firebase_config import get_firebase_config
 
 
-def write_to_firebase(data: dict, ref_id: str, level: str):
+def write_to_firebase(data: dict, ref_id: str, level: str) -> None:
+    """Writes data to Firebase using `https://github.com/thisbejim/Pyrebase <https://stackoverflow.com/a/37484053/11878567>`_
+    , a simply Python wrapper around the Firebase API.
+
+    :param data: A dictionary of Firebase project configuration from
+        :func:`firebase_config.get_firebase_config`
+    :type data: dict
+    :param ref_id: A unique ID where the data would be pushed to on Firebase
+    :type ref_id: str
+    :param level: This should be either ``epoch``, ``batch`` or ``prediction``
+        corresponding to the level where the logs are collected. For ``prediction``,
+        the data would be pushed without the epoch or batch number it was collected on.
+    :type level: str
+    """
+
     # level can be epoch, batch, prediction
     firebase = pyrebase.initialize_app(get_firebase_config())
     log_db = firebase.database()
