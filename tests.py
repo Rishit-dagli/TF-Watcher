d12ae02e16a25989e3a69ed092e1cff87c679d79
Harsh Sharma <sharsh2222@gmail.com>
2021-08-06 18:02:26 +0530

    Update in Tests For Firebase support

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from logging import log
-
+import pyrebase
 import numpy as np
 import pytest
 import tensorflow as tf
@@ -12,6 +12,8 @@ from tfwatcher.callbacks.predict import PredictEnd
 from tfwatcher.callbacks.predict_batch import PredictBatchEnd
 from tfwatcher.callbacks.test_batch import TestBatchEnd
 from tfwatcher.callbacks.train_batch import TrainBatchEnd
+from tfwatcher.firebase_config import get_firebase_config
+from tfwatcher.firebase_helpers import write_to_firebase,write_in_callback,random_char
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
@@ -107,8 +109,44 @@ def test_batch_bool(log_history):
     assert bool_val[0] == False
 
 
+
+data={'accuracy': [0.07400000095367432,
+  0.06466666609048843,
+  0.09316666424274445,
+  0.14800000190734863,
+  0.20475000143051147],
+ 'avg_time': [1.13, 0.35, 0.33],
+ 'batch': [False, False, False],
+ 'epoch': [0, 1, 3],
+ 'loss': [2.369312286376953,
+  2.2976276874542236,
+  2.2363133430480957,
+  2.180738925933838,
+  2.128960371017456],
+ 'time': [[1.1304969787597656],
+  [0.3471100330352783],
+  [0.33536386489868164, 0.32643795013427734]],
+ 'val_accuracy': [0.06466666609048843,
+  0.08299999684095383,
+  0.1420000046491623,
+  0.19233334064483643,
+  0.23633334040641785],
+ 'val_loss': [2.311211347579956,
+  2.2492246627807617,
+  2.193131446838379,
+  2.140815496444702,
+  2.091247797012329]
+  }
+
+def test_firebase_write(data):
+    ref_id = random_char(7)
+    write_to_firebase(data=data,ref_id=ref_id,level='epoch')
+    assert len(ref_id)==7
+
+
 test_assert_len_acc(epochs, log_history)
 test_assert_len_loss(epochs, log_history)
 test_batch_bool(log_history)
+test_firebase_write(data=data)
 
 assert_array_almost_equal(y_test, np.array(predictions_list, dtype="uint8"))
---
+++ tfwatcher/__init__.py
@@ -0,0 +1,1 @@
+from .callbacks import epoch, predict, predict_batch, train_batch
