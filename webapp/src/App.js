731434916edfb3ccb18cd392135b601c68f3c91f
unknown <srparadkar15@gmail.com>
2021-08-06 10:22:15 +0530

    Add 404 page

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Switch, Route, Redirect,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import UserProvider from './providers/AuthProvider';
 import ChartScreen from './screens/ChartScreen';
 import HomeScreen from './screens/HomeScreen';
+import PageNotFound from './screens/404Screen';
 
 const App = () => (
   <UserProvider>
@@ -12,6 +15,8 @@ const App = () => (
       <Switch>
         <Route path="/logs/:id" component={ChartScreen} />
         <Route exact path="/" component={HomeScreen} />
+        <Route path="/404" component={PageNotFound} />
+        <Redirect from="*" to="/404" />
       </Switch>
     </BrowserRouter>
   </UserProvider>
