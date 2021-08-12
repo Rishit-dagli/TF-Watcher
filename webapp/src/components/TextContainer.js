63bba6323a7313fe4d1aaf48d226d37f6a912782
unknown <srparadkar15@gmail.com>
2021-08-12 19:08:48 +0530

    Add docs link to webapp

--- webapp/src/components/TextContainer.js
+++ webapp/src/components/TextContainer.js
@@ -25,17 +25,19 @@ const TextContainer = () => (
           GitHub
         </Button>
       </a>
-      <Button
-        bgColor="gray.700"
-        marginRight={{ base: '0', md: '5' }}
-        marginBottom={{ base: '3', md: '0' }}
-        width="48"
-        boxShadow="lg"
-        leftIcon={<FaRegListAlt />}
-        className="btn"
-      >
-        Documentation
-      </Button>
+      <a href="https://rishit-dagli.github.io/TF-Watcher/" target="_blank" rel="noreferrer">
+        <Button
+          bgColor="gray.700"
+          marginRight={{ base: '0', md: '5' }}
+          marginBottom={{ base: '3', md: '0' }}
+          width="48"
+          boxShadow="lg"
+          leftIcon={<FaRegListAlt />}
+          className="btn"
+        >
+          Documentation
+        </Button>
+      </a>
       <Button
         bgColor="gray.700"
         width="48"
