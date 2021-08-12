11373b6c1a077bc2999acb52a687e4986e83cc5b
unknown <srparadkar15@gmail.com>
2021-08-12 12:24:37 +0530

    Update 404 page UI

--- webapp/src/screens/404Screen.js
+++ webapp/src/screens/404Screen.js
@@ -4,8 +4,8 @@ import { VStack, Text, Button } from '@chakra-ui/react';
 
 const PageNotFound = () => (
   <VStack paddingX="4" textAlign="center">
-    <Text fontSize="3xl" color="teal.600" fontWeight="bold">Oops!</Text>
-    <Text fontSize="lg" fontWeight="medium">The page you are looking for does not exist</Text>
+    <Text fontSize="3xl" color="teal.400" fontWeight="bold">Oops!</Text>
+    <Text fontSize="lg" fontWeight="medium" color="white">The page you are looking for does not exist</Text>
     <Link to="/"><Button colorScheme="teal" marginTop="6">Go back home</Button></Link>
   </VStack>
 );
