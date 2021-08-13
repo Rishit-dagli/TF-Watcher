43f9bdab2dc593653173bf7409a4f2f12f33edb3
unknown <srparadkar15@gmail.com>
2021-08-13 09:40:52 +0530

    Move service-worker to root folder

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-import * as serviceWorkerRegistration from './service/serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
